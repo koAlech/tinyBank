a6681b8b82549dac0b70f68aa74a26c65468d49b
Alik Hochner <alik.hochner@gmail.com>
2014-10-22 15:37:52 +0300

    asdf

--- tinyBank/src/com/tinybank/app/MainActivity.java
+++ tinyBank/src/com/tinybank/app/MainActivity.java
@@ -2,15 +2,14 @@ package com.tinybank.app;
 
 import java.util.Locale;
 
-import android.app.Activity;
 import android.app.ActionBar;
+import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
-import android.support.v13.app.FragmentPagerAdapter;
 import android.os.Bundle;
+import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
